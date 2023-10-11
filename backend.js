4409f6d302a45e458a9ef1b2f6e86bbdbbfd89fb
Senay Tilahun <senay.argaw@gmail.com>
2023-10-11 14:02:23 -0700

    applied the body parsing middleware in Express.js server

--- backend.js
+++ backend.js
@@ -8,5 +8,6 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static("frontend"));
 app.use("/api", apiRouter);
+app.use(express.json());
 
 app.listen(PORT, () => console.log(`First listening on port ${PORT}`));
