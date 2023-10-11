e743c442195b6532e3613194f3c4489a0284210a
Senay Tilahun <senay.argaw@gmail.com>
2023-10-11 13:59:50 -0700

    added put data endpoint to update a restaurant's amenities

--- routes/api.js
+++ routes/api.js
@@ -11,4 +11,26 @@ router.get("/restaurants", async (req, res) => {
   res.json(restaurants);
 });
 
+router.put("/restaurants/:id", async (req, res) => {
+  const restaurantId = req.params.id;
+  // console.log("API - request body ... ", req.body);
+  const updatedAmenities = req.body.updatedAmenities;
+  // console.log("after API - request body ... ", req.body);
+
+  try {
+    const result = await myDB.updateRestaurantAmenities(
+      restaurantId,
+      updatedAmenities
+    );
+    if (result) {
+      res.status(200).json({ message: "Amenties updated successfully" });
+    } else {
+      res.status(404).json({ error: "Restaurant not found" });
+    }
+  } catch (error) {
+    console.log("Error updating amenities:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
 export default router;
