7a4f8aaf820c1015bda10a459c18658d2e00b3a2
Senay Tilahun <senay.argaw@gmail.com>
2023-10-15 20:54:44 -0700

    added aboutus & addew api routers to server

--- backend.js
+++ backend.js
@@ -1,6 +1,8 @@
 import express from "express";
 
 import apiRouter from "./routes/api.js";
+import apiAddNewRouter from "./routes/apiAddNew.js";
+import apiAboutUsRouter from "./routes/apiAboutUs.js";
 
 const app = express();
 
@@ -9,5 +11,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.static("frontend"));
 app.use("/api", apiRouter);
+app.use("/apiAddNew", apiAddNewRouter);
+app.use("/apiAboutUs", apiAboutUsRouter);
 
 app.listen(PORT, () => console.log(`First listening on port ${PORT}`));
