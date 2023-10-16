8460a53cffc8fa250f1ffec834af4aeef4f98220
Senay Tilahun <senay.argaw@gmail.com>
2023-10-15 20:51:45 -0700

    added aboutus & addnew js scripts to html files

--- frontend/js/aboutus.js
+++ frontend/js/aboutus.js
@@ -1,6 +1,51 @@
 function AboutUs() {
   const me = {};
 
+  me.contactListener = () => {
+    const form = document.querySelector("form");
+    form.addEventListener("submit", async (event) => {
+      event.preventDefault();
+      const formData = new FormData(form);
+      const contactData = {
+        name: formData.get("field1"),
+        email: formData.get("field2"),
+        message: formData.get("field3"),
+      };
+
+      try {
+        const response = await fetch("/apiAboutUs/user-message", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(contactData),
+        });
+        if (response.ok) {
+          displayMessage(
+            "Thank you for your message. We will get back to you soon.",
+            30000
+          ); // 30 seconds
+          form.reset(); // Optional: Reset the form after successful submission
+        } else {
+          displayMessage("An error occurred. Please try again later.", 30000); // 30 seconds
+        }
+      } catch (error) {
+        console.error("Error sending message:", error);
+        displayMessage("An error occurred. Please try again later.", 30000); // 30 seconds
+      }
+    });
+  };
+
+  const displayMessage = (message, duration) => {
+    const messageBox = document.createElement("div");
+    messageBox.textContent = message;
+    messageBox.classList.add("message");
+    document.body.appendChild(messageBox);
+    setTimeout(() => {
+      messageBox.remove();
+    }, duration);
+  };
+
   return me;
 }
 
--- frontend/js/addnew.js
+++ frontend/js/addnew.js
@@ -1,6 +1,54 @@
 function AddNew() {
   const me = {};
 
+  me.addRestaurantListener = () => {
+    const form = document.querySelector("form");
+    form.addEventListener("submit", async (event) => {
+      event.preventDefault();
+      const formData = new FormData(form);
+      const newRestaurant = {
+        cafeName: formData.get("cafename"),
+        cafeAddress: formData.get("cafeaddress"),
+        hasOutlet: formData.get("outlet") === "outlet",
+        hasRestroom: formData.get("restroom") === "restroom",
+        hasWifi: formData.get("wifi") === "wifi",
+      };
+
+      try {
+        const response = await fetch("/apiAddNew/add-pending-restaurant", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(newRestaurant),
+        });
+        // const data = await response.json();
+        if (response.ok) {
+          displayMessage(
+            "Thank you for submitting this new restaurant, our admin team will vet this restaurant and we will add it to the our featured list soon. Please feel free to add another!",
+            30000
+          ); // 30 seconds
+          form.reset(); // Optional: Reset the form after successful submission
+        } else {
+          displayMessage("An error occurred. Please try again later.", 30000); // 30 seconds
+        }
+      } catch (error) {
+        console.error("Error adding new restaurant:", error);
+        displayMessage("An error occurred. Please try again later.", 30000); // 30 seconds
+      }
+    });
+  };
+
+  const displayMessage = (message, duration) => {
+    const messageBox = document.createElement("div");
+    messageBox.textContent = message;
+    messageBox.classList.add("message");
+    document.body.appendChild(messageBox);
+    setTimeout(() => {
+      messageBox.remove();
+    }, duration);
+  };
+
   return me;
 }
 
